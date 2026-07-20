aa123ac2f4811b7045c2b21e44c5b3de807b1183
TrroJay <trrojay@163.com>
2026-07-20 23:38:28 +0800

    Adjust markdown content spacing

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -1,6 +1,16 @@
 function userMarkdownSetup(md) {
   // The md parameter stands for the markdown-it instance used throughout the site generator.
   // Feel free to add any plugin you want here instead of /.eleventy.js
+  const defaultText =
+    md.renderer.rules.text ||
+    ((tokens, idx) => md.utils.escapeHtml(tokens[idx].content));
+
+  md.renderer.rules.text = function (tokens, idx, options, env, self) {
+    return defaultText(tokens, idx, options, env, self).replace(
+      /\t/g,
+      '<span class="md-tab" aria-hidden="true"></span>'
+    );
+  };
 }
 function userEleventySetup(eleventyConfig) {
   // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
